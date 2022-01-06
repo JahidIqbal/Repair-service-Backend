44990f911da688c401289d2dbc0de74e6488bec4
hrid620 <hridishere@gmail.com>
2022-01-06 20:52:43 +0600

    all clear

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
 
 // middleware
 app.use(cors());
